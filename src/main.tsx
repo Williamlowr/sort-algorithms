e2f026e79ee232295e272bed37227b9d2ea0846a
William <lowrwilliam@gmail.com>
2025-12-10 00:07:42 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="bg-slate-800/90 min-h-screen flex items-start justify-center">
+    <div className="bg-slate-700/90 min-h-screen flex items-start justify-center">
       <SortVisualizer />
     </div>
   </React.StrictMode>
